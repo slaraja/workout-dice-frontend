4c6acd789edc0cb6d276c294e7b8027e459f99de
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-15 15:06:28 -0400

    basic html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-<script src="src/index.js" charset="utf-8"></script>
 
 document.addEventListener('DOMContentLoaded', () => {
     const endPoint = 'http://localhost:3000/dice_sets';
     fetch(endPoint)
-    .then(res => res.json())
+    .then(resp => resp.json())
     .then(json => console.log(json));
 });
