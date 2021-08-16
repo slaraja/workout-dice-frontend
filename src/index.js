0d53d3a284ffd802f8e71e354f0a5d1124e1584a
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-16 08:31:21 -0400

    fetch dice sets

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(resp => resp.json())
     .then(json => console.log(json));
 });
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const endPoint = 'http://localhost:3000/dice_sets';
+//     fetch(endPoint)
+//     .then(resp => resp.json())
+//     .then(json => console.log(json));
+//     json.forEach(dice_set => {
+//         const workout = `
+//         <li>
+//           <h3>${dice_set.name}
+//             <button>Add</button>
+//           </h3>
+//         </li>`;
+
+//         document.querySelector('#dice_set-list').innerHTML += markup;
+// });
