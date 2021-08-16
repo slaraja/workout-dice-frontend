4093d94859f854618a2a50c8930b42bed809270a
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-16 09:31:07 -0400

    dicesets constructor set up

---
+++ src/Dice_Set.js
@@ -0,0 +1,8 @@
+class DiceSets {
+    constructor(id, name, side, workouts) {
+        this.id = id;
+        this.name = name;
+        this.side = side;
+        this.workouts = workouts;
+    }
+}
