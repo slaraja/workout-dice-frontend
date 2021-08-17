01be0a7c24e9a2d213172924e466c077fb5c4d63
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-17 08:25:34 -0400

    render diceSet code

--- src/Dice_Set.js
+++ src/Dice_Set.js
@@ -4,26 +4,26 @@ class DiceSets {
         this.name = name;
         this.side = side;
         this.workouts = workouts;
-    };
+    }
 
-    function renderDiceSets(arg){
+    renderDiceSets(arg){
         const diceSet = arg["dice"]
         diceSets.forEach(dice => {
         renderDiceSet(dice)
         })
     }
 
-    // renderDiceSet() {
-    //     const diceSetList = document.getElementById('dice-list')
-    //     const diceSetDiv = document.createElement('div')
-    //     diceSetList.classList.add('dice-card')
+
+    renderDiceSet() {
+        const diceSetList = document.getElementById('dice-list')
+        const diceSetDiv = document.createElement('div')
+        diceSetList.classList.add('dice-card')
         
-    //     const diceSetName = document.createElement('h1');
-    //     diceSetName.innerText = this.name;
+        const diceSetName = document.createElement('h1');
+        diceSetName.innerText = this.name;
 
-    //     diceSetCard.appendChild(diceSetName)
-    //     diceSetList.appendChild(diceSetCard)
+        diceSetCard.appendChild(diceSetName)
+        diceSetList.appendChild(diceSetCard)
+    };
+    };
 
-
-
-    //     };
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const endPoint = 'http://localhost:3000/dice_sets';
     fetch(endPoint)
     .then(resp => resp.json())
-    .then(json => console.log(json));
+    .then(json => console.log(json))
+})
 
-    diceSets.forEach[dice => {
-    const newDice = new DiceSet(dice)
-    newDice.renderDiceSet()
-    };
+
+
+    // diceSets.forEach[dice => {
+    // const newDice = new DiceSet(dice)
+    // newDice.renderDiceSet()
