1202f50e614e85a6e77e6bc1f5231d66c2731e32
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-18 09:02:50 -0400

    create dicesetservices file

---
+++ src/DiceSetServices.js
@@ -0,0 +1,16 @@
+class DiceSetServices {
+
+    getDiceSets(){
+    document.addEventListener('DOMContentLoaded', () => {
+        const endPoint = 'http://localhost:3000/dice_sets';
+        fetch(endPoint)
+        .then(resp => resp.json())
+        .then(json => {
+            json.forEach(dice => {
+             const diceSet = new DiceSets(dice);
+              diceSet.renderDiceSet()
+            })
+        })
+        })
+    }
+}
