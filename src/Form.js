83aa997dfedb267776424b495bb0b96379ba51cd
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-24 09:43:38 -0400

    add a workout

--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,24 @@
 
 const form = document.getElementById('form');
-const formBtn = document.querySelector('button');
-const nameInput = document.getElementbyId('name-input');    
+// const formBtn = document.querySelector('button');
+// const nameInput = document.getElementById('name-input');
 // const list = document.getElementById('form-list');
-const nameInputString = nameInput.value
 
 
-formBtn.addEventListener('submit', handleSubmit);
+// formBtn.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit);
 
 
 function handleSubmit(event){
     event.preventDefault() //otherwise will send a post request
     // console.log("clicked")
+    const nameInputField = event.target.querySelector('#name-input')
+    const nameInputString = nameInputField.value
+
+    const exercisePTag = document.createElement('p')
+    exercisePTag.textContent = nameInputString
+
+    form.appendChild(exercisePTag)
 
 }
 
