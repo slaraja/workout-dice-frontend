298b75b594d1b5464cff726a144690010737c4cc
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-27 07:06:27 -0400

    change diceset and diceset services to workout

---
+++ src/Workout.js
@@ -0,0 +1,41 @@
+class Workout {
+    // constructor({id, name, side, workouts}) {
+    //     this.id = id;
+    //     this.name = name;
+    //     this.side = side;
+    //     this.workouts = workouts;
+    // }
+
+    // // renderDiceSets(arg){
+    // //     const diceSet = arg["dice"]
+    // //     diceSets.forEach(dice => {
+    // //     renderDiceSet(dice)
+    // //     })
+    // // }
+
+    // // renderDiceSet() {
+    // //     const diceSetList = document.getElementById('dice-list')
+    // //     diceSetList.classList.add('dice')
+        
+    // //     const diceSetName = document.createElement('h1');
+    // //     diceSetName.innerText = this.name;
+
+    // //     diceSetList.appendChild(diceSetName)
+    // // };
+    
+    // renderDiceSetWorkouts() {
+    //     const diceSetList = document.getElementById('dice-list')
+    //     diceSetList.classList.add('dice')
+
+    //     const diceSetWorkouts = document.createElement('h2');
+    //     diceSetWorkouts.innerText = this.workouts;
+    //     debugger
+    //     diceSetList.appendChild(diceSetWorkouts)
+
+    // }
+
+
+};
+
+
+
---
+++ src/WorkoutServices.js
@@ -0,0 +1,17 @@
+class WorkoutServices {
+    
+    // constructor(baseURL) {
+    //     this.baseURL = baseURL;
+    // };
+    
+    // getDiceSets(){
+    //     fetch(this.baseURL) 
+    //     .then(resp => resp.json())
+    //     .then(json => {
+    //         json.forEach(dice => {
+    //             const dicesets = new DiceSet(dice);
+    //         dicesets.renderDiceSet()
+    //         })
+    //     })
+    // }
+}
