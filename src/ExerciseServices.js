8d65b5965d886bb249d9ec813e813fccbfee132d
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-27 13:50:09 -0400

    update getExercises

--- src/ExerciseServices.js
+++ src/ExerciseServices.js
@@ -1,5 +1,22 @@
 class ExerciseServices {
 
+    constructor(baseURL) {
+    this.baseURL = baseURL;
+   };
+
+    getExercises(){
+            fetch(this.baseURL)
+            .then(resp => resp.json())
+            .then(json => {
+                json.forEach(workout => {
+                 const workouts = new Exercise(exercise);
+                  exercises.renderExercise()
+                })
+            })
+        }
+
+}
+
 //    constructor(baseURL) {
 //        this.baseURL = baseURL;
 //    };
