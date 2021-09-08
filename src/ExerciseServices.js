8ae3ed8ad6bbdba17548e5991425ddecbf6c51f3
Stephanie La Raja <stephanielaraja@gmail.com>
2021-09-08 17:04:51 -0400

    remove comments from exerciseservices

--- src/ExerciseServices.js
+++ src/ExerciseServices.js
@@ -1,15 +1,12 @@
 class ExerciseServices {
 
-//handle submit gets an event obj arg automatically
     handleSubmit(event){
-        event.preventDefault() //otherwise will send a post request
+        event.preventDefault() 
         const nameInputField = document.getElementById('name-input')
         const repsInputField = document.getElementById('reps-input')
         const workoutInputField = document.getElementById('workout-dropdown')
         const form = document.getElementById('form');
 
-
-    //grab the value of what the user types into the field
         const inputs = {
             name: nameInputField.value,
             reps: repsInputField.value,
@@ -24,27 +21,21 @@ class ExerciseServices {
             },
             body: JSON.stringify(inputs)
         }
+        
         console.log("configObj", configObj)
         fetch('http://localhost:3000/exercises', configObj)
         .then(resp => resp.json())
         //parses JSON into a JS object. It returns a promise.
         .then(json => {
-            
             const newExercise = new Exercise(json)
             const workoutList = document.getElementById(`workout-${newExercise.workout_id}`)
-            //find a specific workout instead of a list
             const appendedExercise = newExercise.renderExercise()
-            // debugger
-
             workoutList.appendChild(appendedExercise)
-
             })
-            form.reset()
 
-        // })
+            form.reset()
      
     }
-
 
 }
 
